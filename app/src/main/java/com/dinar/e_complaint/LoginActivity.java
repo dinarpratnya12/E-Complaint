b727fb8565e8f6835dfbb893c0cc51e4c8e6e41e
Dinarpratnya12 <Dinarpratnya12@gmail.com>
2019-10-18 15:13:09 +0700

    Alikasi pengajuan complaint🌚🌝

---
+++ app/src/main/java/com/dinar/e_complaint/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.dinar.e_complaint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView txt;
+    Button btnLogin;
+    EditText edtEmail, edtPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPass);
+        btnLogin = findViewById(R.id.btnLogin);
+        txt = findViewById(R.id.txt1);
+
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/dinar/e_complaint/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dinar.e_complaint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
