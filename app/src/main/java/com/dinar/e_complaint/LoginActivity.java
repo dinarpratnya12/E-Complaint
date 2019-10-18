91130aaca07ff4c75a519ac44891d66ab0f5d4ad
Dinarpratnya12 <Dinarpratnya12@gmail.com>
2019-10-18 17:02:59 +0700

    New update🌞

--- app/src/main/java/com/dinar/e_complaint/LoginActivity.java
+++ app/src/main/java/com/dinar/e_complaint/LoginActivity.java
@@ -1,19 +1,31 @@
 package com.dinar.e_complaint;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView txt;
     Button btnLogin;
     EditText edtEmail, edtPass;
+    private FirebaseAuth mAuth;
+    String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,34 @@ public class LoginActivity extends AppCompatActivity {
         edtPass = findViewById(R.id.edtPass);
         btnLogin = findViewById(R.id.btnLogin);
         txt = findViewById(R.id.txt1);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String pass = edtPass.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent login = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(login);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
+            }
+        });
 
         txt.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/dinar/e_complaint/MainActivity.java
+++ app/src/main/java/com/dinar/e_complaint/MainActivity.java
@@ -3,12 +3,44 @@ package com.dinar.e_complaint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtComplaint;
+    Button btnSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_input_complaint);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("message");
+
+        edtComplaint = findViewById(R.id.edtComplaint);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String complaint = edtComplaint.getText().toString();
+                myRef.child("complaint").setValue(complaint).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "Data Berhasil Masuk",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
     }
 }
